f6c5db38eb895513f622cec2afb67449bb40612a
Alvin Lin <alvinlin88@gmail.com>
2018-11-03 11:24:13 -0700

    Initial commit

---
+++ src/main/java/voterinformation/handlers/HelpIntentHandler.java
@@ -0,0 +1,28 @@
+package voterinformation.handlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.request.Predicates;
+import static com.amazon.ask.request.Predicates.intentName;
+
+import java.util.Optional;
+
+
+
+public class HelpIntentHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(intentName("AMAZON.HelpIntent"));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "You have opened voting information. How can I help you?";
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("Voter Information", speechText)
+                .build();
+    }
+}
