ab67453c492b1b7d05bd55deb6f0d7ecc7ff7509
Preet Patel <ppatel39@asu.edu>
2018-11-03 13:09:11 -0700

    Merge branch 'master' of /Users/Preet/Documents/GitHub/AlexaVoterInfo with conflicts.

--- src/main/java/voterinformation/handlers/LaunchRequestHandler.java
+++ src/main/java/voterinformation/handlers/LaunchRequestHandler.java
@@ -17,7 +17,7 @@ public class LaunchRequestHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+        String speechText = "Welcome to the Versed Voter. How may I help you?";
         return input.getResponseBuilder()
                 .withSpeech(speechText)
                 .withSimpleCard("HelloWorld", speechText)
