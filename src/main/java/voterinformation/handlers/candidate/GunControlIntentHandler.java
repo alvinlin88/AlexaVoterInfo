c151c4782651bb3c8c457411cac6c888387b1388
Alvin Lin <alvinlin88@gmail.com>
2018-11-03 19:44:26 -0700

    intents

---
+++ src/main/java/voterinformation/handlers/candidate/GunControlIntentHandler.java
@@ -0,0 +1,37 @@
+package voterinformation.handlers.candidate;
+
+import java.util.*;
+
+import com.amazon.ask.model.interfaces.display.*;
+import voterinformation.util.votingInfo;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class GunControlIntentHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(intentName("GunControlIntent").or(intentName("AMAZON.YesIntent")));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+
+        Map<String, String> candidateMap = votingInfo.getInfo2();
+
+        String candidate = candidateMap.get("Candidate");
+        String speechText = candidate;
+
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("Versed Voter", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
+
+}
