3cb80057361a742af27784ade8731474144310a6
Alvin Lin <alvinlin88@gmail.com>
2018-11-04 11:37:56 -0700

    Adding Google's Civil Info API for Voting Information

--- src/main/java/voterinformation/VoterInformationStreamHandler.java
+++ src/main/java/voterinformation/VoterInformationStreamHandler.java
@@ -5,6 +5,8 @@ import com.amazon.ask.Skills;
 import com.amazon.ask.SkillStreamHandler;
 import voterinformation.handlers.*;
 import voterinformation.handlers.amendment.*;
+import voterinformation.handlers.apidemo.MyAddressIsIntent;
+import voterinformation.handlers.apidemo.VotingLocationIntentHandler;
 import voterinformation.handlers.candidate.*;
 import voterinformation.handlers.judge.*;
 
@@ -32,6 +34,9 @@ public class VoterInformationStreamHandler extends SkillStreamHandler {
                         new JudgeFusonIntentHandler(),
                         new JudgeMoodySpecialIntentHandler(),
                         new JudgeMoodyIntentHandler(),
+
+                        new MyAddressIsIntent(),
+                        new VotingLocationIntentHandler(),
 
                         new AmendmentIntentHandler(),
                         new AmendmentMeaningIntentHandler(),
